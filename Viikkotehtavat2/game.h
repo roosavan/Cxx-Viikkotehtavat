711b2b5baec2c882b97e4b61e94fae37951f7b04
roosavan <roosavan@gmail.com>
2025-02-05 16:55:31 +0200

    Viikkotehtavat2

---
+++ Viikkotehtavat2/game.h
@@ -0,0 +1,19 @@
+#ifndef GAME_H
+#define GAME_H
+
+class Game
+{
+public:
+    Game(int maxNumber);
+    ~Game();
+    void play();
+    void printGameResult();
+
+private:
+    int maxNumber;
+    int playerGuess;
+    int randomNumber;
+    int numOfGuesses;
+};
+
+#endif // GAME_H
